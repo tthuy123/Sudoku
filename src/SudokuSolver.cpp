4eead7ea216843aa683884eeb04c649ea91846c4
thuthuy <125456003+tthuy123@users.noreply.github.com>
2025-04-18 15:09:13 +0700

    refactor: standardize variable names in SudokuSolver class for consistency

--- src/SudokuSolver.cpp
+++ src/SudokuSolver.cpp
@@ -1,28 +1,26 @@
 #include "SudokuSolver.h"
 
 Sudoku::Solver::Solver()
-	: mGrid(nullptr), mGenModifier(false), mNumToIgnore({ 0, 0, 0 })
+	: grid(nullptr), genModifier(false), numToIgnore({ 0, 0, 0 })
 {
 
 }
 
-//--------------------------------------Private methods----------------------------------------//
 inline void Sudoku::Solver::setElement(const int row, const int col, const int num)
 {
-	mGrid[row * 9 + col] = num;
+	grid[row * SIZE + col] = num;
 }
 
 inline int Sudoku::Solver::getElement(const int row, const int col) const
 {
-	return mGrid[row * 9 + col];
+	return grid[row * SIZE + col];
 }
 
 bool Sudoku::Solver::checkValid(const int inputRow, const int inputCol, const int num) const
 {
-	// if number is valid i.e. not found in row, col, or block then return true
 
 	// check row
-	for (int col = 0; col < 9; col++)
+	for (int col = 0; col < SIZE; col++)
 	{
 		if (getElement(inputRow, col) == num)
 		{
@@ -31,7 +29,7 @@ bool Sudoku::Solver::checkValid(const int inputRow, const int inputCol, const in
 	}
 
 	// check col
-	for (int row = 0; row < 9; row++)
+	for (int row = 0; row < SIZE; row++)
 	{
 		if (getElement(row, inputCol) == num)
 		{
@@ -64,27 +62,27 @@ bool Sudoku::Solver::checkValid(const int inputRow, const int inputCol, const in
 //--------------------------------------Public methods----------------------------------------//
 void Sudoku::Solver::setGenModifier(const bool input)
 {
-	mGenModifier = input;
+	genModifier = input;
 }
 
 void Sudoku::Solver::setGrid(int* grid, const Ignore& ignoreInput)
 {
-	mNumToIgnore = ignoreInput;
-	mGrid = grid;
+	numToIgnore = ignoreInput;
+	this->grid = grid;
 }
 
 bool Sudoku::Solver::solve()
 {
-	for (int row = 0; row < 9; row++)
+	for (int row = 0; row < SIZE; row++)
 	{
-		for (int col = 0; col < 9; col++)
+		for (int col = 0; col < SIZE; col++)
 		{
 			if (getElement(row, col) == 0)
 			{
 				for (int num = 1; num < 10; num++)
 				{
 					// If generator modifier is set to true and the row, col and number match then skip to next iteration
-					if (!(mGenModifier && mNumToIgnore.row == row && mNumToIgnore.col == col && mNumToIgnore.num == num))
+					if (!(genModifier && numToIgnore.row == row && numToIgnore.col == col && numToIgnore.num == num))
 					{
 						// if number is valid i.e. not found in row, col, or block then return true
 						if (checkValid(row, col, num))
@@ -111,11 +109,11 @@ bool Sudoku::Solver::solve()
 
 void Sudoku::Solver::display() const
 {
-	for (int row = 0; row < 9; row++)
+	for (int row = 0; row < SIZE; row++)
 	{
-		for (int col = 0; col < 9; col++)
+		for (int col = 0; col < SIZE; col++)
 		{
-			std::cout << mGrid[row * 9 + col] << ", ";
+			std::cout << grid[row * SIZE + col] << ", ";
 		}
 		std::cout << std::endl;
 	}
